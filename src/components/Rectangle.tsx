67c6f8597f558cf0bfa7fe373ba2ec2c018cd88f
Steve Schroeder <sschroeder@fr.com>
2019-03-05 09:54:59 -0600

    Help from Goose

--- src/components/Rectangle.tsx
+++ src/components/Rectangle.tsx
@@ -13,46 +13,48 @@ export interface IState {
   rectangle: IRectangle;
 }
 
-function Rectangle(props: IRectangle, state: IState) {
-  return (
-    <div className="ParameterDiv" id="Rectangle">
-      <input
-        type="number"
-        min="1"
-        name="length"
-        className="form-control"
-        placeholder="Length"
-        value={props.height}
-      />
-      <br />
-      <input
-        type="number"
-        min="1"
-        name="width"
-        className="form-control"
-        placeholder="Width"
-        value={props.width}
-      />
-      <br />
-      <input
-        type="number"
-        min="1"
-        name="perimeter"
-        className="form-control"
-        placeholder="Perimeter"
-        value={props.perimeter}
-      />
-      <br />
-      <input
-        type="number"
-        min="1"
-        name="area"
-        className="form-control"
-        placeholder="Area"
-        value={props.area}
-      />
-    </div>
-  );
+class Rectangle extends React.Component<{}, IState>{
+  public render() {
+    return (
+      <div className="ParameterDiv" id="Rectangle">
+        <input
+          type="number"
+          min="1"
+          name="length"
+          className="form-control"
+          placeholder="Length"
+          value={this.state.rectangle.height}
+        />
+        <br />
+        <input
+          type="number"
+          min="1"
+          name="width"
+          className="form-control"
+          placeholder="Width"
+          value={this.state.rectangle.width}
+        />
+        <br />
+        <input
+          type="number"
+          min="1"
+          name="perimeter"
+          className="form-control"
+          placeholder="Perimeter"
+          value={this.state.rectangle.perimeter}
+        />
+        <br />
+        <input
+          type="number"
+          min="1"
+          name="area"
+          className="form-control"
+          placeholder="Area"
+          value={this.state.rectangle.area}
+        />
+      </div>
+    );
+  }
 }
 
 export default Rectangle;
